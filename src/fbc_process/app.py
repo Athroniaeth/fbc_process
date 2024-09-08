dded037a743a899d9f7dd5190404cb602e10f8e7
Athroniaeth <pierre.chaumont@hotmail.fr>
2024-09-08 13:45:16 +0200

    Refactor interface, add estimation of cost

--- src/fbc_process/app.py
+++ src/fbc_process/app.py
@@ -9,6 +9,27 @@ import typer
 
 from fbc_process.process import process_df
 
+PRICE_1M_INPUT_TOKENS = 2.7
+PRICE_1M_OUTPUT_TOKENS = 8.2
+
+
+def estimate_price(input_token: int, output_token: int) -> float:
+    """
+    Estimate the price for the process.
+
+    Args:
+        input_token (int): The number of input tokens.
+        output_token (int): The number of output tokens.
+
+    Returns:
+        float: The estimated price.
+    """
+    calcul_input = (input_token / 1_000_000) * PRICE_1M_INPUT_TOKENS
+    calcul_output = (output_token / 1_000_000) * PRICE_1M_OUTPUT_TOKENS
+    calcul = calcul_input + calcul_output
+
+    return calcul
+
 
 def _gradio_process(
         list_path: list,
@@ -24,9 +45,10 @@ def _gradio_process(
     Returns:
         pandas.DataFrame: The resulting DataFrame.
     """
-    df_final = pd.DataFrame()
     total_input_token = 0
     total_output_token = 0
+    length = len(list_path)
+    df_final = pd.DataFrame()
 
     for path in list_path:
         df = pandas.read_excel(path)
@@ -37,19 +59,26 @@ def _gradio_process(
         total_input_token += input_token
         total_output_token += output_token
 
+    calcul = estimate_price(total_input_token, total_output_token)
+    logging.info(f"Estimated price: {calcul:.8f} €")
+
+    calcul_nbr_request = (1 / calcul) / length
+    logging.info(f"Number request for 1€ (by file): {calcul_nbr_request:.2f}")
+
     return df_final, total_input_token, total_output_token
 
 
 def app(
-    debug: bool = typer.Option(False, envvar="DEBUG", help="Enable the debug mode."),
-    host: str = typer.Option("127.0.0.1", envvar="HOST", help="Adress to listen on."),
-    port: int = typer.Option(7860, envvar="PORT", help="Port to listen on."),
-    ssl_keyfile: str = typer.Option(None, envvar="SSL_KEYFILE", help="File containing the SSL key."),
-    ssl_certfile: str = typer.Option(None, envvar="SSL_CERTFILE", help="File containing the SSL certificate."),
-    model_id: str = typer.Option("mistral-large-latest", help="Model Mistral to use."),
-    mistral_token: str = typer.Option(None, envvar="MISTRAL_API_KEY", help="Token to access the Mistral API."),
-    max_file_size: str = typer.Option("10mb", envvar="MAX_FILE_SIZE", help="Size of the maximum file to download."),
-    enable_monitoring: bool = typer.Option(True, envvar="ENABLE_MONITORING", help="Activate the monitoring of the application."),
+        debug: bool = typer.Option(False, envvar="DEBUG", help="Enable the debug mode."),
+        host: str = typer.Option("127.0.0.1", envvar="HOST", help="Adress to listen on."),
+        port: int = typer.Option(7860, envvar="PORT", help="Port to listen on."),
+        ssl_keyfile: str = typer.Option(None, envvar="SSL_KEYFILE", help="File containing the SSL key."),
+        ssl_certfile: str = typer.Option(None, envvar="SSL_CERTFILE", help="File containing the SSL certificate."),
+        model_id: str = typer.Option("mistral-large-latest", help="Model Mistral to use."),
+        mistral_token: str = typer.Option(None, envvar="MISTRAL_API_KEY", help="Token to access the Mistral API."),
+        max_file_size: str = typer.Option("10mb", envvar="MAX_FILE_SIZE", help="Size of the maximum file to download."),
+        enable_monitoring: bool = typer.Option(True, envvar="ENABLE_MONITORING",
+                                               help="Activate the monitoring of the application."),
 ):
     """
     Start the Gradio server to serve the model.
@@ -73,19 +102,35 @@ def app(
     )
 
     with gr.Blocks() as application:
-        file_input = gr.File(
-            file_count="multiple",
-            label="Upload your Excel file",
-            file_types=[
-                ".xlsx",
-            ],
-        )
-        button = gr.Button()
-        dataframe = gr.DataFrame()
-        info_input_token = gr.Textbox("0", label="Input token")
-        info_output_token = gr.Textbox("0", label="Output token")
+        gr.Markdown("## FBC Process")
+        gr.Markdown(
+            "This application detects the format of Excel files, reformats them and renames columns according to their data type.")
 
-        button.click(fn=gradio_process, inputs=file_input, outputs=[dataframe, info_input_token, info_output_token])
+        with gr.Row():
+            with gr.Column(scale=1):
+                file_input = gr.File(
+                    file_count="multiple",
+                    label="Upload your Excel file",
+                    file_types=[
+                        ".xlsx",
+                    ],
+                )
+                button = gr.Button()
+                info_input_token = gr.Textbox("0", label="Input token")
+                info_output_token = gr.Textbox("0", label="Output token")
+
+            with gr.Column(scale=4):
+                dataframe = gr.DataFrame()
+
+        button.click(
+            fn=gradio_process,
+            inputs=file_input,
+            outputs=[
+                dataframe,
+                info_input_token,
+                info_output_token
+            ]
+        )
 
     application.launch(
         ssl_verify=False,
